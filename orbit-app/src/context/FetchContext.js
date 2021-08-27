33db1fc67dd84f69e75451af3abbfd63616e4e3d
Thanh Nguyen <vnscriptkid@gmail.com>
2021-08-27 22:46:12 +0700

    Config axios instance with jwt token in headers

--- orbit-app/src/context/FetchContext.js
+++ orbit-app/src/context/FetchContext.js
@@ -1,18 +1,28 @@
-import React, { createContext } from 'react';
-import axios from 'axios';
+import React, { createContext, useContext } from "react";
+import axios from "axios";
+import { AuthContext } from "./AuthContext";
 
 const FetchContext = createContext();
 const { Provider } = FetchContext;
 
 const FetchProvider = ({ children }) => {
+  const authContext = useContext(AuthContext);
   const authAxios = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: process.env.REACT_APP_API_URL,
   });
+
+  authAxios.interceptors.request.use(
+    (config) => {
+      config.headers.Authorization = `Bearer ${authContext.authState.token}`;
+      return config;
+    },
+    (error) => Promise.reject(error)
+  );
 
   return (
     <Provider
       value={{
-        authAxios
+        authAxios,
       }}
     >
       {children}
